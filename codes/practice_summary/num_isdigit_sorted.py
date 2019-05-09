57a7f91e1a28052c1ca79f7fa5872ca2523bc91c
duanhaobin <1807227469@qq,com>
2019-05-10 01:07:44 +0800

    修改限定比较三个整数大小的算法

--- codes/practice_summary/num_isdigit_sorted.py
+++ codes/practice_summary/num_isdigit_sorted.py
@@ -4,14 +4,16 @@
 '''
 输入三个整数x,y,z，请把这三个数由小到大输出，可调用input()。（需要加判断：判断输入数据是否为数字）
 '''
-def num_sort(n):
+
+def num_sort2():
     result_lst = []
-    for i in range(1,n+1):
+    count = 0
+    while(count < 3): # 限定比较三个整数
         in_str = input('请输入一个整数:')
         while(not (in_str.isdigit())):
             in_str = input('输入有误,请重新输入:')
-        result_lst.append(int(in_str))
+        else:
+            count += 1
+            result_lst.append(int(in_str))
     return sorted(result_lst)
-
-print(num_sort(3))
-
+print('三个整数比较后的结果为:',num_sort2())
